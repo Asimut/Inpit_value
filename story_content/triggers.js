f01b7dcd72e52e44bbf6629d1afa97e04cc078fe
Oleh <asimut007@gmail.com>
2024-05-20 13:30:16 +0300

    5255

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,8 +2,38 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5fDiBWJmfDX":
+      case "6nvfDcWgYdh":
         Script1();
+        break;
+      case "67RpEyJwY3c":
+        Script2();
+        break;
+      case "6EQomsNRo6w":
+        Script3();
+        break;
+      case "6cjlYAFBPoA":
+        Script4();
+        break;
+      case "5a8ZyllmsvZ":
+        Script5();
+        break;
+      case "5xRdtCji8E3":
+        Script6();
+        break;
+      case "6SIx3SKJHGg":
+        Script7();
+        break;
+      case "681yDs0u9eN":
+        Script8();
+        break;
+      case "6lsqTiDJQcv":
+        Script9();
+        break;
+      case "6JrlJTj0QfT":
+        Script10();
+        break;
+      case "6CWdsQfcyQx":
+        Script11();
         break;
   }
 }
@@ -15,4 +45,84 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5ydpz4BIjrQ');
+const duration = 750;
+const easing = 'ease-out';
+const id = '68GlhL0WSIO';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5ydpz4BIjrQ');
+const duration = 750;
+const easing = 'ease-out';
+const id = '68GlhL0WSIO';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  player.once(() => {
+const target = object('6meNzAWhq4x');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5cOujT39HQ1';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script4 = function()
+{
+  const target = object('6meNzAWhq4x');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5cOujT39HQ1';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
 };
--- story_content/user.js
+++ story_content/user.js
@@ -5,42 +5,134 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
-window.Script1 = function()
+window.Script5 = function()
 {
-// Функция для проверки, работает ли код на мобильном устройстве
-function isMobile() {
-    return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
-}
+  var head = document.getElementsByTagName('head')[0];
 
-// Функция для вставки из буфера обмена
-function pasteFromClipboard(event) {
-    // Проверка, что длительное нажатие (1 секунда)
-    setTimeout(function() {
-        navigator.clipboard.readText().then(function(text) {
-            // Найти поле ввода и вставить текст из буфера обмена
-            var inputField = document.querySelector('[data-dv_ref="input"]');
-            if (inputField) {
-                inputField.value = text;
-                // Обновить переменную Articulate
-                SetPlayerVariable("TextEntry", text);
-            }
-        }).catch(function(err) {
-            console.error('Failed to read clipboard contents: ', err);
-        });
-    }, 1000); // 1 секунда
-}
-
-// Добавление обработчика событий на поле ввода
-document.addEventListener('DOMContentLoaded', function() {
-    if (isMobile()) {
-        var inputField = document.querySelector('[data-dv_ref="input"]');
-        if (inputField) {
-            inputField.addEventListener('touchstart', pasteFromClipboard);
-        }
-    }
-});
-
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.7.1.min.js';
+script.onload = function() {
     
+ 
+    trigger2();
+};
+head.appendChild(script);
+
+function trigger2() {
+    var bDay = document.querySelector('input[aria-label="bDay"]');
+    
+    bDay.addEventListener('input', function(e) {
+        var value = bDay.value;
+        var numbers = value.replace(/\D/g, '');
+
+        var chars = numbers.split('');
+        var formattedValue = '';
+
+        for (var i = 0; i < chars.length; i++) {
+            if (i === 2 || i === 4) {
+                formattedValue += '.';
+            }
+            formattedValue += chars[i];
+        }
+
+        bDay.value = formattedValue.substring(0, 10);
+    });
+}
+
+}
+
+window.Script6 = function()
+{
+  var player = GetPlayer();
+var inputPhone = document.querySelector('input[aria-label="phone"]');
+console.log(inputPhone);
+
+inputPhone.addEventListener('input', function(e) {
+    var value = inputPhone.value;
+    var numbers = value.replace(/\D/g, '');
+
+
+    numbers = numbers.substring(0, 11);
+
+    var formattedValue = '8';
+    for (var i = 1; i < numbers.length; i++) {
+        if (i === 1) {
+            formattedValue += '';
+        } else if (i === 4) {
+            formattedValue += '';
+        }
+        formattedValue += numbers[i];
+    }
+    
+    inputPhone.value = formattedValue;
+});
+}
+
+window.Script7 = function()
+{
+  var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+ var player = GetPlayer();
+ var email=player.GetVar("userEmail"); 
+ 
+  var isValidate=false;
+  if (reg.test(email))
+  {
+   var isValidate=true;
+  }
+  else
+  {
+   var isValidate=false;
+  }
+     player.SetVar("userEmailValidation",isValidate);
+}
+
+window.Script8 = function()
+{
+  var input = document.querySelector('textarea[aria-label="issue_date"]');
+    
+    input.addEventListener('input', function(e) {
+        var value = input.value;
+        var numbers = value.replace(/\D/g, '');
+
+        
+        var chars = numbers.split('');
+        var formattedValue = '';
+
+
+        for (var i = 0; i < chars.length; i++) {
+            if (i === 2 || i === 4) {
+                formattedValue += '.';
+            }
+            formattedValue += chars[i];
+        }
+
+        input.value = formattedValue.substring(0, 10);
+    });
+}
+
+window.Script9 = function()
+{
+  var iframes = document.querySelectorAll("iframe");
+console.log(iframes);
+iframes.forEach(function(iframe) {
+    iframe.contentWindow.postMessage("blurInputs", "*");
+});
+}
+
+window.Script10 = function()
+{
+  var iframes = document.querySelectorAll("iframe");
+iframes.forEach(function(iframe) {
+    iframe.contentWindow.postMessage("blurInputs", "*");
+});
+}
+
+window.Script11 = function()
+{
+  var iframes = document.querySelectorAll("iframe");
+iframes.forEach(function(iframe) {
+    iframe.contentWindow.postMessage("blurInputs", "*");
+});
 }
 
 };
